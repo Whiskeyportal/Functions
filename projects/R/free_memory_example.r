0d2adbb77cc1d3c91d5f835dec0799894cd2021c
Morgan <whiskeyportal@gmail.com>
2019-04-17 09:36:31 -0600

    Create free_memory_example.r

---
+++ projects/R/free_memory_example.r
@@ -0,0 +1,3 @@
+gc() # Force R to release memory it is no longer using
+ls() # Lists all the objects in your current workspace
+rm(list = ls()) # If you want to delete all the objects in the workspace and start with a clean slate
